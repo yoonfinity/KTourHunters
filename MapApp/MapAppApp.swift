aecc5dacd2cdcb5e6654d5fdfa83195aaa25c65a
Jason <janghyukyoon@gmail.com>
2025-11-17 10:44:53 +0900

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Jason Yoon on 11/17/25.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
